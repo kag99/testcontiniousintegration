ac9445ae67f9d07840c79c91e7893474c7b5023f
kag99 <kag99@rediffmail.com>
2022-03-29 11:58:49 +0530

    Test commit

---
+++ TestCI/TestCI/TestCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCIApp.swift
+//  TestCI
+//
+//  Created by Amit Gowda on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TestCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
